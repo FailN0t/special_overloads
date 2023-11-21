a45afa30fd96dcfdc78bedaacc08b555b40077a5
Igor Putyajkin <putyajkin99@gmail.com>
2023-11-21 19:14:11 +0300

    ini5t

--- special_overloads/special_overloads.cpp
+++ special_overloads/special_overloads.cpp
@@ -2,10 +2,108 @@
 //
 
 #include <iostream>
+#include <time.h>
+using namespace std;
+
+class Stroka {
+    int* arr;
+    const int size;
+public:
+    // Stroka(int size)
+    Stroka(int size) :arr{ new int[size] }, size{ size } {
+        for (size_t i = 0; i < size; i++)
+        {
+            arr[i] = rand() % 10 + 1;
+        }
+    }
+    Stroka() :Stroka(2) {}
+
+    //int len() const  return size (Stroka);
+    int len() const {
+        return size;
+    }
+    int& operator [] (int i) {
+        return arr[i];
+    }
+    int operator [] (int i) const {
+        return arr[i];
+    }
+    ~Stroka() {
+        cout << arr << " delete class: " << this << endl;
+        delete[] arr;
+    }
+};
+
+
+class Mtr {
+    Stroka* stroka;
+    int size;
+public:
+    Mtr(int size) :stroka{ new Stroka[size]}, size{size} {}
+    Mtr() : Mtr(5) {}
+    //int len() const  return size (Mtr);
+    int len() const {
+        return size;
+    }
+    Stroka& operator [] (int i) {
+        return stroka[i];
+    }
+    Stroka operator [] (int i) const {
+        return stroka[i];
+    }
+
+    void operator << (Mtr& mtr) const {
+        for(size_t i = 0; i < mtr.len(); i++)
+        {
+            for (size_t j = 0; j < mtr[i].len(); j++)
+            {
+                cout << mtr[i][j] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    friend ostream& operator << (ostream& ost, Mtr& mtr) {
+        for (size_t i = 0; i < mtr.len(); i++)
+        {
+            for (size_t j = 0; j < mtr[i].len(); j++)
+            {
+                ost << mtr[i][j] << "\t";
+            }
+            ost << endl;
+        }
+        ost << endl;
+        return ost;
+    }
+
+    friend istream& operator >> (istream& ist, Mtr& mtr) {
+        for (size_t i = 0; i < mtr.len(); i++)
+        {
+            for (size_t j = 0; j < mtr[i].len(); j++)
+            {
+                ist >> mtr[i][j];
+            }
+        }
+        return ist;
+    }
+
+    ~Mtr() {
+        cout << stroka << " delete class Mtr : " << this << endl;
+        delete[] stroka;
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mtr mtr;
+    mtr << mtr;
+
+    cout << mtr;
+    Mtr mtr2(2);
+    cin >> mtr2;
+    cout << mtr2;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
